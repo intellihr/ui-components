4a66cfb012b443c81c6a69694205a24016dc2a8b
Nick Luo <nick.luo@me.com>
2018-03-07 10:51:07 +1000

    Initial project setup

---
+++ src/Button/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+export interface ButtonProps {
+    children?: any;
+    className?: string;
+    type?: string;
+    onClick?: React.ReactEventHandler<any>;
+}
+
+export const Button: React.SFC<ButtonProps> = props => (
+    <button> Sample Button </button>
+)
+
