faef3915728fc8176119f0fa67e31d50e386dcf9
Nick Luo <nick.luo@me.com>
2018-03-07 13:51:47 +1000

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+module.exports = {
+  resolve: {
+    extensions: ['.ts', '.tsx', '.jsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /(node_modules)/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              compilerOptions: {
+                "outDir": "./",
+                "jsx": "react",
+              }
+            }
+          },
+          'stylelint-custom-processor-loader',
+        ],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          'url-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map'
+}
