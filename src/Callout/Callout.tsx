11fe35335dd6a7d88fb445efdbce688c017bedcd
Nick Luo <nick.luo@me.com>
2018-03-08 10:47:37 +1000

    Add Callout Component

---
+++ src/Callout/Callout.tsx
@@ -0,0 +1,57 @@
+import classNames from 'classnames';
+import { isEmpty } from 'lodash'
+import React from 'react'
+
+export class Callout extends React.Component <any, any> {
+  get messages () {
+    const { message } = this.props
+    let { messages } = this.props
+
+    if (isEmpty(messages)) {
+      messages = []
+    }
+
+    if (message) {
+      messages.push(message)
+    }
+
+    return messages
+  }
+
+  get classNames(): string[] {
+    const {
+      justifyCenter,
+      type
+    } = this.props
+
+    const cNames = [
+      'callout',
+      type
+    ]
+
+    if (justifyCenter) {
+      cNames.push('justify-center')
+    }
+
+    return cNames
+  }
+
+  public render () {
+    const { children } = this.props
+
+    if (isEmpty(this.messages) && !children) {
+      return null
+    }
+
+    return (
+      <div
+        className={classNames(this.classNames)}
+        ref="callout"
+      >
+        <div className="content">
+          {children}
+        </div>
+      </div>
+    )
+  }
+}
