03b2db8f343f88cc04cf1f47f1652b2b67904fc6
Nick Luo <nick.luo@me.com>
2018-03-08 15:55:11 +1000

    Add config for building Sass in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,24 +6,18 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /(node_modules)/,
-        enforce: 'pre',
-        loader: 'tslint-loader',
-      },
+      // {
+      //   test: /\.(ts|tsx)$/,
+      //   exclude: /(node_modules)/,
+      //   enforce: 'pre',
+      //   loader: 'tslint-loader',
+      // },
       {
         test: /\.(ts|tsx)$/,
         exclude: /(node_modules)/,
         use: [
           {
-            loader: 'ts-loader',
-            options: {
-              compilerOptions: {
-                "outDir": "./",
-                "jsx": "react",
-              }
-            }
+            loader: 'awesome-typescript-loader'
           }
         ],
       },
@@ -31,6 +25,47 @@ module.exports = {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         use: [
           'url-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: () => [
+                require('postcss-cssnext')()
+              ]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              // Related to: https://github.com/webpack-contrib/sass-loader/issues/351
+              outputStyle: 'compact',
+              includePaths: [
+                path.resolve(__dirname, 'node_modules/foundation-sites/scss'),
+                path.resolve(__dirname, 'node_modules'),
+                path.resolve(__dirname, 'node_modules/compass-mixins/lib')
+              ]
+            }
+          },
+          {
+            loader: '@epegzz/sass-vars-loader',
+            options: {
+              files: [
+                path.resolve(__dirname, 'src/styles/colours.js')
+              ]
+            }
+          }
         ]
       }
     ]
