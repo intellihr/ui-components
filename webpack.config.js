8a091b22f39e4058b03be0fea1c43f336c959496
Nick Luo <nick.luo@me.com>
2018-03-19 17:31:28 +1000

    Update webpack config for fonts

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/',    // where the fonts will go
+            publicPath: '../'       // override the default path
+          }
+        }]
+      },
+      {
         test: /\.(ts|tsx)$/,
         exclude: /(node_modules)/,
         enforce: 'pre',
