35c6540737ec21cfe5f1dacb01f4ba34828bb2d7
Constantin Oleinic <olcod.dev@gmail.com>
2018-04-11 10:23:30 +1000

    Added the person profile picture component

---
+++ src/Badge/PersonBadge.examples.md
@@ -0,0 +1,122 @@
+## Person Badge examples
+```jsx
+<div>
+  <PersonBadge
+    preferredOrFirstName='John'
+    lastName='Wick'
+    size='xlarge'
+  />
+  <PersonBadge
+    preferredOrFirstName='Zoltan'
+    lastName='Khevai'
+    size='large'
+  />
+  <PersonBadge
+    preferredOrFirstName='Edward'
+    lastName='Nigma'
+    size='medium'
+  />
+  <PersonBadge
+    preferredOrFirstName='Tom'
+    lastName='Hanks'
+    size='small'
+  />
+
+  <PersonBadge
+    preferredOrFirstName='John'
+    lastName='Wick'
+    size='xlarge'
+    isOnLeave
+  />
+  <PersonBadge
+    preferredOrFirstName='Zoltan'
+    lastName='Khevai'
+    size='large'
+    isOnLeave
+  />
+  <PersonBadge
+    preferredOrFirstName='Edward'
+    lastName='Nigma'
+    size='medium'
+    isOnLeave
+  />
+  <PersonBadge
+    preferredOrFirstName='Tom'
+    lastName='Hanks'
+    size='small'
+    isOnLeave
+  />
+
+  <br />
+
+  <PersonBadge
+    preferredOrFirstName='John'
+    lastName='Wick'
+    size='xlarge'
+    imageId='https://pbs.twimg.com/profile_images/921785062626127873/FbfY4sRz_400x400.jpg'
+    handleClick={() => {}}
+  />
+  <PersonBadge
+    preferredOrFirstName='Zoltan'
+    lastName='Chivay'
+    size='large'
+    imageId='https://vignette.wikia.nocookie.net/witcher/images/4/4d/Tw2_zoltan_chivay_render.jpg/revision/latest/top-crop/width/240/height/240?cb=20170605185340'
+    handleClick={() => {}}
+  />
+  <PersonBadge
+    preferredOrFirstName='Edward'
+    lastName='Nigma'
+    size='medium'
+    imageId='http://style.anu.edu.au/_anu/4/images/placeholders/person.png'
+    handleClick={() => {}}
+  />
+  <PersonBadge
+    preferredOrFirstName='Tom'
+    lastName='Hanks'
+    size='small'
+    imageId='http://style.anu.edu.au/_anu/4/images/placeholders/person.png'
+    handleClick={() => {}}
+  />
+
+  <PersonBadge
+    preferredOrFirstName='John'
+    lastName='Wick'
+    size='xlarge'
+    isOnLeave
+    imageId='https://pbs.twimg.com/profile_images/921785062626127873/FbfY4sRz_400x400.jpg'
+    hoverIcon='camera'
+    hoverLabel='Change Picture'
+    handleClick={() => {}}
+  />
+  <PersonBadge
+    preferredOrFirstName='Zoltan'
+    lastName='Chivay'
+    size='large'
+    isOnLeave
+    imageId='https://vignette.wikia.nocookie.net/witcher/images/4/4d/Tw2_zoltan_chivay_render.jpg/revision/latest/top-crop/width/240/height/240?cb=20170605185340'
+    hoverIcon='camera'
+    hoverLabel='Change Picture'
+    handleClick={() => {}}
+  />
+  <PersonBadge
+    preferredOrFirstName='Edward'
+    lastName='Nigma'
+    size='medium'
+    isOnLeave
+    imageId='http://style.anu.edu.au/_anu/4/images/placeholders/person.png'
+    hoverIcon='camera'
+    hoverLabel='Change Picture'
+    handleClick={() => {}}
+  />
+  <PersonBadge
+    preferredOrFirstName='Tom'
+    lastName='Hanks'
+    size='small'
+    isOnLeave
+    imageId='http://style.anu.edu.au/_anu/4/images/placeholders/person.png'
+    hoverIcon='camera'
+    hoverLabel='Change Picture'
+    handleClick={() => {}}
+  />
+</div>
+```
---
+++ src/Badge/PersonBadge.tsx
@@ -0,0 +1,163 @@
+import React from 'react'
+import classNames from 'classnames'
+import { isNil } from 'lodash'
+const style = require('./PersonBadge.scss')
+
+import { Icon } from '../Icon/Icon'
+
+export interface SkeletonOptions {
+  showSkeleton: boolean
+  shape: string
+}
+
+export interface PersonBadgeProps {
+  preferredOrFirstName?: string
+  lastName?: string
+  className?: string
+  hoverLabel?: string
+  hoverIcon?: string
+  handleClick?: (event: React.MouseEvent<HTMLDivElement>) => void
+  size?: 'original' | 'small' | 'medium' | 'large' | 'xlarge'
+  imageId?: string
+  imageData?: string
+  isOnLeave?: boolean
+  skeletonOptions?: SkeletonOptions
+  onImageError?: (error: any) => void
+}
+
+export interface PersonBadgeState {
+  showInitials: boolean
+}
+
+export class PersonBadge extends React.Component<PersonBadgeProps> {
+  public state: PersonBadgeState = { showInitials: true }
+
+  public static defaultProps: PersonBadgeProps = {
+    size: 'large',
+    isOnLeave: false,
+    onImageError: error => {
+      console.log('Something went wrong with the image: ', error)
+    }
+  }
+
+  constructor (props: any) {
+    super (props)
+
+    this.state = {
+      showInitials: !this.hasImage(props)
+    }
+  }
+
+  protected hasImage (props: any): boolean {
+    const {
+      imageId,
+      imageData
+    } = props
+
+    return !isNil(imageId) || !isNil(imageData)
+  }
+
+  get hoverDom (): JSX.Element | null {
+    const {
+      size,
+      hoverLabel,
+      hoverIcon
+    } = this.props
+
+    if (isNil(hoverLabel) && isNil(hoverIcon)) {
+      return null
+    }
+
+    return (
+      <div className={classNames('person-badge-hover', size)}>
+        <Icon type='camera' /> 
+        <span className='person-badge-hover-label'>{hoverLabel}</span>
+      </div>
+    )
+  }
+
+  get initials (): string {
+    const {
+      preferredOrFirstName,
+      lastName
+    } = this.props
+
+    const firstInitial = preferredOrFirstName ? preferredOrFirstName.charAt(0) : ''
+    const lastInitital = lastName ? lastName.charAt(0) : ''
+
+    return firstInitial + lastInitital
+  }
+
+  get picture (): JSX.Element {
+    const {
+      imageId,
+      imageData,
+      onImageError
+    } = this.props
+
+    return (
+      <img
+        src={imageId ? imageId : imageData}
+        onError={ event => { isNil(onImageError) ? null : onImageError(event) } }
+      />
+    )
+  }
+
+  get badgeContent (): JSX.Element {
+    if (this.state.showInitials) {
+      return (
+        <div className='badge-initials-container'>
+          <span className='badge-intials'>
+            {this.initials}
+          </span>
+        </div>
+      )
+    }
+
+    return (
+      <div className='badge-picture-container'>
+        {this.picture}
+      </div>
+    )
+  }
+
+  get extendedLeaveDot (): JSX.Element | null {
+    const {
+      isOnLeave
+    } = this.props
+
+    if (isOnLeave) {
+      return <span className='extended-leave-circle' />
+    }
+
+    return null
+  }
+
+  public render (): JSX.Element | null {
+    const {
+      className,
+      size,
+      handleClick
+    } = this.props
+
+    return (
+      <div className={classNames(
+        style.PersonBadge,
+        className,
+        size
+      )}>
+        <div 
+          className={classNames(
+            'person-badge-inner-container',
+            { 'with-hover': !isNil(handleClick) }
+          )}
+          onClick={event => isNil(handleClick) ? null : handleClick(event)}
+        >
+          {this.hoverDom}
+          {this.badgeContent}
+        </div>
+        {this.extendedLeaveDot}
+      </div>
+    )
+  }
+}
