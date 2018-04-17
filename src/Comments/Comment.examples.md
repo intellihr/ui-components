3d0fa165ecdaa69e1ffa04aff22cf9c673a27477
Constantin Oleinic <olcod.yndx@yandex.com>
2018-04-17 13:39:08 +1000

    Almost done with the comments, most likely will need to add another component for comment header

--- src/Comments/Comment.examples.md
+++ src/Comments/Comment.examples.md
@@ -1,4 +1,5 @@
 ## Comment
+### Someone else's comment
 ```jsx
   <Comment
     comment={{
@@ -6,13 +7,51 @@
       comment: 'This is the first comment ever',
       personDisplayName: 'Testing Preson Name',
       personFirstName: 'Culkin',
-      personPreferedOrFirstName: 'McKooley',
+      personPreferredOrFirstName: 'McKooley',
       createdAt: '2017-01-02 08:10:59',
-      personId: 'sampleCommentPersonId',
-      label: <div>This is a test</div>
+      personId: 'sampleCommentPersonId'
     }}
-    loggedUser={{
-      id: 'sampoleLoggedUserId'
+    loggedInUser={{
+      id: 'sampleLoggedUserId'
+    }}
+    idx={1}
+  />
+```
+
+### Own comment
+```jsx
+  <Comment
+    comment={{
+      id: 'sampleCommentId',
+      comment: 'This is the first comment ever',
+      personDisplayName: 'Testing Preson Name',
+      personFirstName: 'Culkin',
+      personPreferredOrFirstName: 'McKooley',
+      createdAt: '2017-01-02 08:10:59',
+      personId: 'sampleCommentPersonId'
+    }}
+    loggedInUser={{
+      id: 'sampleCommentPersonId'
+    }}
+    idx={1}
+  />
+```
+
+### Own comment with a header
+```jsx
+  <Comment
+    comment={{
+      id: 'sampleCommentId',
+      comment: 'This is the first comment ever',
+      header: <div></div>
+      personDisplayName: 'Testing Preson Name',
+      personFirstName: 'Culkin',
+      personPreferredOrFirstName: 'McKooley',
+      createdAt: '2017-01-02 08:10:59',
+      personId: 'sampleCommentPersonId'
+    }}
+    loggedInUser={{
+      id: 'sampleCommentPersonId'
     }}
     idx={1}
   />
--- src/Comments/Comment.tsx
+++ src/Comments/Comment.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import classNames from 'classnames'
-
 import { PersonBadge } from '../PersonBadge/PersonBadge'
+const style = require('./style.scss')
 
 export interface CommentProps {
   comment: {
     id: string,
     comment: string,
-    header?: React.Component,
+    header?: JSX.Element,
     personDisplayName: string,
-    personFirtsName: string,
+    personFirstName: string,
     personPreferredOrFirstName: string,
     createdAt: string,
     personId: string,
     personProfilePictureId?: string,
-    label: React.Component | JSX.Element
+    label?: JSX.Element
   }
   loggedInUser: {
     id: string
@@ -24,6 +24,123 @@ export interface CommentProps {
 }
 
 export class Comment extends React.Component<CommentProps> {
+  get personProfilePicture (): JSX.Element {
+    const {
+      personFirstName,
+      personPreferredOrFirstName,
+      personProfilePictureId
+    } = this.props.comment
+
+    return (
+      <div className='comment-badge-container'>
+        <PersonBadge
+          size='small'
+          preferredOrFirstName={personPreferredOrFirstName}
+          lastName={personFirstName}
+        />
+      </div>
+    )
+  }
+
+  get ownerLabel (): JSX.Element | null {
+    const {
+      comment: {
+        personId: commenterId
+      },
+      loggedInUser: {
+        id: loggedInUserId
+      }
+    } = this.props
+
+    if (commenterId === loggedInUserId) {
+      return <div className='comment-header-owner-label'>Owner</div>
+    }
+
+    return null
+  }
+
+  get commentDate (): JSX.Element {
+    return (
+      <div className='comment-header-date'>
+        25 minutes ago
+      </div>
+    )
+  }
+
+  get commenterName (): JSX.Element {
+    const {
+      comment: {
+        personId,
+        personDisplayName
+      },
+      loggedInUser: {
+        id: loggedInUserId
+      }
+    } = this.props
+
+    console.log(personId, loggedInUserId)
+
+    return (
+      <strong className='comment-header-person-name'>
+        {personId === loggedInUserId ? 'You' : personDisplayName}
+      </strong>
+    )
+  }
+
+  get commentHeaderMeta (): JSX.Element | null {
+    const {
+      comment: {
+        header
+      }
+    } = this.props
+
+    if (header) {
+      return header
+    }
+
+    return null
+  }
+
+  get commentHeaderLabel (): JSX.Element | null {
+    const {
+      comment: {
+        label
+      }
+    } = this.props
+
+    if (label) {
+      return label
+    }
+
+    return null
+  }
+
+  get commentHeader (): JSX.Element {
+    return (
+      <div className='comment-header-container'>
+        {this.commenterName}
+        {this.commentHeaderMeta}
+
+        {this.ownerLabel}
+        
+        {this.commentDate}
+        {this.commentHeaderLabel}
+      </div>
+    )
+  }
+
+  get commentContent (): JSX.Element {
+    const {
+      comment
+    } = this.props.comment
+
+    return (
+      <div className='comment-content'>
+        {comment}
+      </div>
+    )
+  }
+
   render (): JSX.Element {
     const {
       comment
@@ -32,13 +149,11 @@ export class Comment extends React.Component<CommentProps> {
     } = this.props
 
     return (
-      <div id={comment.id} className={classNames()}>
-        <div className='comment-badge-container'>
-          <PersonBadge
-            size='small'
-            preferredOrFirstName={comment.personPreferredOrFirstName}
-            lastName={comment.personFirtsName}
-          />
+      <div id={comment.id} className={classNames(style.Comment)}>
+        {this.personProfilePicture}
+        <div className='comment-content-container'>
+          {this.commentHeader}
+          {this.commentContent}
         </div>
       </div>
     )
