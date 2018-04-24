881bc97701a5dd58178eb3d21f0e28176d2c208c
Nick Luo <nick.luo@me.com>
2018-04-24 14:10:49 +1000

    Fix tests

--- src/Colour/Colour.test.tsx
+++ src/Colour/Colour.test.tsx
@@ -2,6 +2,7 @@ import { mount } from 'enzyme'
 import React from 'react'
 import { brandColours } from './Colour'
 import { Colour } from './'
+import 'jest-styled-components'
 
 describe('<Colour />', () => {
   it(`should render a colour component`, () => {
--- src/Colour/ColoursGrid.test.tsx
+++ src/Colour/ColoursGrid.test.tsx
@@ -2,6 +2,7 @@ import { mount } from 'enzyme'
 import React from 'react'
 import { ColoursGrid } from './'
 import { brandColoursArray } from './Colour'
+import 'jest-styled-components'
 
 describe('<ColoursGrid />', () => {
   it(`should render a colour component`, () => {
