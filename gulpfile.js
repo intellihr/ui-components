ab859685597807034f34fb8018bbabcacb626267
Nick Luo <nick.luo@me.com>
2018-05-02 00:04:53 +1000

    Some clean up

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const concat = require('gulp-concat');
-
-gulp.task('sass', () => {
-  return gulp.src('./src/sass/app.scss')
-    // .pipe((things) => console.log('things is ', things))
-    .pipe(gulp.dest('destination'))
-})
