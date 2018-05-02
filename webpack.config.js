fbb6085d2fc06e758334cbbe3e3e5350404fbc9f
Nick Luo <nick.luo@me.com>
2018-05-02 10:50:31 +1000

    Some refactorings

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   ],
   entry: {
     index: './src/index.ts',
-    base: './src/sass/app.scss'
+    ['ui-components']: './src/sass/app.scss'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
