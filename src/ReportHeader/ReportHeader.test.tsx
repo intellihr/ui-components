80625777f7bcb8bd3a1bba22bf4c20ce9a411809
Nick Luo <nick.luo@me.com>
2018-05-24 16:54:50 +1000

    Rename to ReportHeader

---
+++ src/ReportHeader/ReportHeader.test.tsx
@@ -0,0 +1,19 @@
+import { mount } from 'enzyme'
+import React from 'react'
+import { ReportHeader } from './'
+
+describe('<ReportHeader />', () => {
+  it(`should render ReportHeader`, () => {
+    const wrapper = mount(
+      <ReportHeader
+        primaryText='How To Read This Chart'
+        secondaryText='Hide Info'
+        title={'Hello'}
+        displayInfo={true}
+        renderHelperContent={() => (<span> 'I am help text' </span>)}
+      />
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+}
