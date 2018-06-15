0356918ec0a2ee6e4bdf51337a32619f9adf2c9c
Sean Manson <sean.manson@intellihr.com.au>
2018-06-15 13:00:24 +1000

    Linting fix

--- src/FormattedText/style.ts
+++ src/FormattedText/style.ts
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled,  { StyledComponentClass } from 'styled-components'
+import styled, { StyledComponentClass } from 'styled-components'
 
 export const StyledFormattedText = styled.div`
   white-space: pre-wrap;
