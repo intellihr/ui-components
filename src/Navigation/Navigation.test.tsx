3e4b031f1b26ac108109bc5d6f1b598248387700
Nick Luo <nick.luo@me.com>
2018-06-19 18:24:50 +1000

    Add tests

---
+++ src/Navigation/Navigation.test.tsx
@@ -0,0 +1,23 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import { Navigation } from './Navigation'
+import { MenuItem } from '../Menu'
+import { IntelliIcon } from '../Icon'
+
+describe('<Navigation />', () => {
+  it(`should render a simple navigation Menu`, () => {
+    const wrapper = shallow(
+      <Navigation>
+        <MenuItem 
+          url="/#"
+          label="Dashboard" 
+          icon={<IntelliIcon type={'clock'} />}
+        />
+      </Navigation>
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+})
