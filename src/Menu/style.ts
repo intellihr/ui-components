bb91452286daf882f4e1c62b97d061161cec6eb9
Nick Luo <nick.luo@me.com>
2018-06-19 11:44:39 +1000

    Remove unused variable

--- src/Menu/style.ts
+++ src/Menu/style.ts
@@ -5,13 +5,6 @@ export const MenuItemAnchor = styled.a`
   padding: 1 rem;
 `
 
-export const styleMenuIcon = (Icon: React.ComponentType) => styled(Icon)`
-  margin-right: 0.1825rem;
-  width: 1.28571em;
-  text-align: center;
-  vertical-align: top;
-`
-
 export const StyledIcon = styled.span`
   margin-right: 0.2rem;
   width: 1.28571em;
