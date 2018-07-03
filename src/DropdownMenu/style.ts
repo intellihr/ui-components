0a4fa0a8b4e454d374d75bc8dd2d2d1b0c7a6b76
Michael Devenish <michael@michaeldevenish.com>
2018-07-03 13:54:06 +1000

    cleanup style

--- src/DropdownMenu/style.ts
+++ src/DropdownMenu/style.ts
@@ -3,22 +3,24 @@ import styled, { StyledComponentClass } from 'styled-components'
 import { getColor } from '../Color'
 
 const DefaultDropdownButton = styled.button`
-  padding: .5rem .3rem;
   background-color: transparent;
-  cursor: pointer;
-  transition: background-color .25s ease-out, color .25s ease-out;
   color: #929fab;
+  cursor: pointer;
+  padding: .5rem .3rem;
+  transition: background-color .25s ease-out, color .25s ease-out;
 
-  &.hover, &:hover, &:active, &:focus {
+  &:hover,
+  &:active,
+  &:focus {
     background-color: ${getColor('neutral-base')};
     color: ${getColor('main-text')};
     outline: none;
   }
   
   .fa {
+    font-size: 1.5rem;
     line-height: 1;
     margin: 0;
-    font-size: 1.5rem;
     width: 1.3em;
   }
 `
