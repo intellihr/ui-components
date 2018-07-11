288c8b834cd1cc025153da272191e8b8950c4d5f
Michael Dorkhom <michael.dorkhom@gmail.com>
2018-07-11 10:42:18 +1000

    Fix styles

--- src/Badge/style.ts
+++ src/Badge/style.ts
@@ -15,37 +15,33 @@ const BadgeWrapper = styled.span`
     color: ${props.color};
   `}
   
+  &, .fa {
   ${(props: IBadgeWrapper) => {
     switch (props.size) {
       case 'small':
         return `
-          &, .fa {
-            font-size: 12px;
-            width: 20px;
-            height: 20px;
-            line-height: 20px;
-          }
+          font-size: 12px;
+          width: 20px;
+          height: 20px;
+          line-height: 20px;
         `
       case 'medium':
         return `
-          &, .fa {
-            font-size: 15px;
-            width: 24px;
-            height: 24px;
-            line-height: 24px;
-          }
+          font-size: 15px;
+          width: 24px;
+          height: 24px;
+          line-height: 24px;
         `
       case 'large':
         return `
-          &, .fa {
-            font-size: 21px;
-            width: 30px;
-            height: 30px;
-            line-height: 30px;
-          }
+          font-size: 21px;
+          width: 30px;
+          height: 30px;
+          line-height: 30px;
         `
     }
   }}
+  }
   
   box-shadow: 0 2px 2px rgba(0,0,0,0.24);
   border: 1px solid $neutral-base;
