8bd96b1ad4504be37abec55f7b2724fa86e98127
Eric Chan <kinyat2009@gmail.com>
2018-07-26 10:37:56 +1000

    [INT-1545] Avoid custom value in DefaultProvider overwrites the default values even if they are not set

---
+++ src/domain/Defaults/Defaults/Defaults.test.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { DefaultsProvider, DefaultsConsumer } from './Defaults'
+
+describe('<Defaults />', () => {
+  it('should provide default values', () => {
+    const mockFn = jest.fn()
+
+    const wrapper = mount(
+      <DefaultsProvider>
+        <DefaultsConsumer>
+          {defaults => mockFn(defaults)}
+        </DefaultsConsumer>
+      </DefaultsProvider>
+    )
+
+    expect(mockFn.mock.calls).toMatchSnapshot()
+  })
+
+  it('should provide values with defaults', () => {
+    const mockFn = jest.fn()
+
+    const wrapper = mount(
+      <DefaultsProvider
+        value={{
+          AnchorComponent: () => <div />
+        }}
+      >
+        <DefaultsConsumer>
+          {defaults => mockFn(defaults)}
+        </DefaultsConsumer>
+      </DefaultsProvider>
+    )
+
+    expect(mockFn.mock.calls).toMatchSnapshot()
+  })
+
+  it('should overwrite default values', () => {
+    const mockFn = jest.fn()
+
+    const wrapper = mount(
+      <DefaultsProvider
+        value={{
+          breakpoints: {
+            small: 0,
+            medium: 100,
+            large: 200,
+            xlarge: 300,
+            xxlarge: 400
+          }
+        }}
+      >
+        <DefaultsConsumer>
+          {defaults => mockFn(defaults)}
+        </DefaultsConsumer>
+      </DefaultsProvider>
+    )
+
+    expect(mockFn.mock.calls).toMatchSnapshot()
+  })
+
+  it('should still provide default values without Provider', () => {
+    const mockFn = jest.fn()
+
+    const wrapper = mount(
+      <DefaultsConsumer>
+        {defaults => mockFn(defaults)}
+      </DefaultsConsumer>
+    )
+
+    expect(mockFn.mock.calls).toMatchSnapshot()
+  })
+})
