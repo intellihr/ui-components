15cdad487df6716f165aa75887a796eb89518deb
Felix Lee <felix.lee@intellihr.com.au>
2018-08-20 12:44:17 +1000

    INT-1975 Updated links colors with new variables

--- src/domain/Links/ActionLink/ActionLink.tsx
+++ src/domain/Links/ActionLink/ActionLink.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styled, { StyledFunction } from 'styled-components'
 import { Anchor, IAnchorProps } from '../../Internals/Anchor/Anchor'
 
+const { i400, i500, i600 } = require('../../../common/sass/variables.scss')
+
 // tslint:disable-next-line:no-empty-interface
 interface IActionLinkProps extends IAnchorProps {}
 
@@ -11,14 +13,23 @@ export const ActionLink = styledAnchor`
   font-weight: 600;
   font-size: .9375rem;
   text-transform: uppercase;
+  transition: color .25s ease-out;
+
 
   &,
   &:link,
-  &:visited,
-  &:hover,
-  &:active {
-    color: rgb(5, 87, 223);
+  &:visited {
+    color: ${i400};
   }
+
+  &:hover {
+    color: ${i500}
+  }
+
+  &:active {
+    color: ${i600}
+  }
+
 `
 
 ActionLink.displayName = 'ActionLink'
