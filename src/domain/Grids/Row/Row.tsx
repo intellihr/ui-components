70dcdda733de63194f5f2792381b3a03fb77a45f
Suchandra Chakraborty <suchandra.2005@gmail.com>
2018-08-24 10:45:55 +1000

    Removed redundant code
    INT-1958

--- src/domain/Grids/Row/Row.tsx
+++ src/domain/Grids/Row/Row.tsx
@@ -2,13 +2,7 @@ import React from 'react'
 import { Row as StyledRow } from 'react-styled-flexboxgrid'
 import { Column } from '../Column'
 
-interface IRoleProps {
-  className?: string
-  onClick?: () => void
-  style?: object
-}
-
-class Row extends React.PureComponent<IRoleProps> {
+class Row extends React.PureComponent<any> {
   public static Column = Column
 
   public render (): JSX.Element {
