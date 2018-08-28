4f036704f55b6e20470717d28783fea1b1c8d9b0
Vivian Lee <vivian@Vivians-MacBook-Pro.local>
2018-08-28 10:18:35 +1000

    pr report info test indent fix
    INT-2073

--- src/domain/Reports/ReportInfo/ReportInfo.test.tsx
+++ src/domain/Reports/ReportInfo/ReportInfo.test.tsx
@@ -4,46 +4,46 @@ import { ReportInfo } from './ReportInfo'
 import { Variables } from '../../../common'
 
 describe('<ReportInfo />', () => {
-    it('should render a ReportInfo', () => {
-        const wrapper = shallow(
-            <ReportInfo
-                description='Info.'
-            />
-        )
+  it('should render a ReportInfo', () => {
+    const wrapper = shallow(
+      <ReportInfo
+        description='Info.'
+      />
+    )
 
-        expect(wrapper).toMatchSnapshot()
-    })
+    expect(wrapper).toMatchSnapshot()
+  })
 
-    it('should render a ReportInfo with text color', () => {
-        const wrapper = shallow(
-            <ReportInfo
-                description='Hello! I am blue report info.'
-                textColor={Variables.Color.i400}
-            />
-        )
+  it('should render a ReportInfo with text color', () => {
+    const wrapper = shallow(
+      <ReportInfo
+        description='Hello! I am blue report info.'
+        textColor={Variables.Color.i400}
+      />
+    )
 
-        expect(wrapper).toMatchSnapshot()
-    })
+    expect(wrapper).toMatchSnapshot()
+  })
 
-    it('should render a ReportInfo with highlights', () => {
-        const wrapper = shallow(
-            <ReportInfo
-                description='Hello! I am blue report info.'
-                highlights={[
-                    {
-                        'caption': 'I am Highlight 1.',
-                        'title': 'highlight 1 title',
-                        'imageUrl': 'https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?ssl=1'
-                    },
-                    {
-                        'caption': 'I am Highlight 2.',
-                        'title': 'highlight 2 title',
-                        'imageUrl': 'https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?ssl=1'
-                    }
-                ]}
-            />
-        )
+  it('should render a ReportInfo with highlights', () => {
+    const wrapper = shallow(
+      <ReportInfo
+        description='Hello! I am blue report info.'
+        highlights={[
+          {
+            'caption': 'I am Highlight 1.',
+            'title': 'highlight 1 title',
+            'imageUrl': 'https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?ssl=1'
+          },
+          {
+            'caption': 'I am Highlight 2.',
+            'title': 'highlight 2 title',
+            'imageUrl': 'https://i1.wp.com/www.foot.com/wp-content/uploads/2017/03/placeholder.gif?ssl=1'
+          }
+        ]}
+      />
+    )
 
-        expect(wrapper).toMatchSnapshot()
-    })
+    expect(wrapper).toMatchSnapshot()
+  })
 })
