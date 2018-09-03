45c45b94e2661e678992250fdf1a6eae7b7d9cf6
Michael Devenish <michael@michaeldevenish.com>
2018-09-04 09:18:15 +1000

    Fix styling of annotated sections
    [BUGFIX]

--- src/domain/Layouts/SectionList/style.ts
+++ src/domain/Layouts/SectionList/style.ts
@@ -3,16 +3,18 @@ import styled, { css } from 'styled-components'
 import { Variables } from '../../../common'
 import { AnnotatedSectionDescription } from './subcomponents/AnnotatedSectionDescription'
 
-const sharedStyle = css`
-  @media only screen and (max-width: ${Variables.Breakpoint.breakpointTablet - 1}px) {
-    padding: 24px 16px;
-  }
-
+const sharedDesktopStyle = css`
   @media only screen and (min-width: ${Variables.Breakpoint.breakpointTablet}px) {
     margin-left: 24px;
     margin-right: 24px;
     padding-top: 32px;
     padding-bottom: 32px;
+  }
+`
+
+const sharedMobileStyle = css`
+  @media only screen and (max-width: ${Variables.Breakpoint.breakpointTablet - 1}px) {
+    padding: 24px 16px;
   }
 `
 
@@ -27,26 +29,41 @@ const SectionBorder = styled.div`
 `
 
 const StyledAnnotatedSectionBody = styled.div`
+  ${sharedMobileStyle}
+
+  @media only screen and (min-width: ${Variables.Breakpoint.breakpointTablet}px) {
+    margin-left: 24px;
+  }
+  
   @media only screen and (max-width: ${Variables.Breakpoint.breakpointTablet - 1}px) {
     background-color: ${Variables.Color.n150};
  
     border-top: 1px solid ${Variables.Color.n300};
     border-bottom: 1px solid ${Variables.Color.n300};
   }
+`
 
-  ${sharedStyle}
+const StyledAnnotatedSectionDescription = styled(AnnotatedSectionDescription)`
+  ${sharedMobileStyle}
+
+  @media only screen and (min-width: ${Variables.Breakpoint.breakpointTablet}px) {
+    margin-right: 24px;
+  }
 `
 
 const StyledSection = styled(SectionBorder)` 
-  ${sharedStyle}
+  ${sharedMobileStyle}
+  ${sharedDesktopStyle}
 `
-const StyledAnnotatedSectionDescription = styled(AnnotatedSectionDescription)`
-  ${sharedStyle}
+
+const StyledAnnotatedSection = styled(SectionBorder)` 
+  ${sharedDesktopStyle}
 `
 
 export {
   SectionBorder,
   StyledSection,
+  StyledAnnotatedSection,
   StyledAnnotatedSectionDescription,
   StyledAnnotatedSectionBody
 }
