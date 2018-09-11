fe707f2a46bc9cc9b3f77dd34d934020dd4b96e7
Kaitlyn Pickard <kaitlyn.pickard@intellihr.com.au>
2018-09-11 10:36:38 +1000

    Add unit test for sections with no children
    int-2324

--- src/domain/Layouts/SectionList/SectionList.test.tsx
+++ src/domain/Layouts/SectionList/SectionList.test.tsx
@@ -35,6 +35,30 @@ describe('<SectionList />', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
+  it('should render sectionList items with no children', () => {
+    const wrapper = mount(
+      <SectionList>
+        <SectionList.Section/>
+        <SectionList.AnnotatedSection
+          header='Annotated Section'
+          description='Annotated Description'
+          linkText='Annotated Link'
+          linkProps={{href: '#'}}
+        />
+        <SectionList.TitledSection
+          header='Titled Section'
+          description='Titled Description'
+          actionItems={[
+            <Button type='primary' key={1}>Action 1</Button>,
+            <Button type='primary' key={2}>Action 2</Button>
+          ]}
+        />
+      </SectionList>
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
   it('should render a section list with an annotated section without props', () => {
     const wrapper = mount(
       <SectionList>
