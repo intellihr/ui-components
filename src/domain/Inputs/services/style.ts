cf6f9c9f8adf5373a9a28ded0316ec4672221b21
Andrew Hill <andrew.hill@intellihr.com.au>
2018-10-22 13:55:13 +1000

    Use ref to find the exact width of the prefix component
    INT-3172

--- src/domain/Inputs/services/style.ts
+++ src/domain/Inputs/services/style.ts
@@ -4,46 +4,43 @@ import { Variables } from '../../../common'
 export interface IInputWrapperProps {
   disabledPrefix?: string
   hasIcon: boolean
+  prefixWrapperWidth: number
 }
+
+const wrapperPadding = 8
 
 const InputWrapper = styled.div`
   position: relative;
   color: ${Variables.Color.n800};
   width: 100%;
-
-  input {
-    text-indent: 30px;
-  }
   
-  ${(props: IInputWrapperProps) => props.disabledPrefix && !props.hasIcon && css`
+  ${(props: IInputWrapperProps) => css`
      input {
-        text-indent: ${props.disabledPrefix.length * 10}px;
+        text-indent: ${props.prefixWrapperWidth - wrapperPadding}px;
       }
   `}
-  
-  ${(props: IInputWrapperProps) => props.disabledPrefix && props.hasIcon && css`
-     input {
-        text-indent: ${props.disabledPrefix.length * 9 + 30}px;
-      }
-  `}
+ 
 `
 const PrefixWrapper = styled.div`
     position: absolute;
     width: auto;
     height: 39px;
-    padding: 8px;
+    padding: ${wrapperPadding}px;
     border: 1px solid transparent;
+    line-height: 16px;
     
     .fa {
     color: ${Variables.Color.n400};
-    margin-left: 8px;
+    line-height: 21px;
+    vertical-align: top;
   }
 `
 
 const DisabledTextWrapper = styled.span`
-    margin-left: 8px;
+    margin-left: 4px;
     line-height: 21px;
     vertical-align: top;
+    user-select: none;
 `
 
 const StyledInput = styled.input`
