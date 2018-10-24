c246bcbcdcac162d8b1900afa28c7e28f30c91fd
Vivian Lee <vivian.lee@intellihr.com.au>
2018-10-24 14:27:56 +1000

    pr comment
    INT-3198

--- src/domain/Forms/VerticalForm/subcomponents/style.ts
+++ src/domain/Forms/VerticalForm/subcomponents/style.ts
@@ -1,8 +1,7 @@
-import React from 'react'
 import styled, {css} from 'styled-components'
 import { Variables } from '../../../../common'
 
-interface IStyledInputLabelProps extends React.HTMLProps<HTMLLabelElement> {
+interface IStyledInputLabelProps {
   isRequired: boolean
 }
 
