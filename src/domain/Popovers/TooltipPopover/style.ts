b3fad5f3f86ede34e7588dc06aaf47f71c7632d7
Michael Devenish <michael@michaeldevenish.com>
2018-11-01 16:33:10 +1000

    Fix text not breaking line in tooltip popovers.
    [INT-3309]

--- src/domain/Popovers/TooltipPopover/style.ts
+++ src/domain/Popovers/TooltipPopover/style.ts
@@ -7,12 +7,16 @@ interface IStyledPopoverProps {
 
 const StyledTooltipContent = styled.div`
   background-color: ${Variables.Color.n100};
-  border: 1px solid ${Variables.Color.n400};
   border-radius: ${Variables.Style.borderRadius}px;
+  border: 1px solid ${Variables.Color.n400};
   box-shadow: 0 2px 4px rgba(38, 45, 51, 0.1);
+  hyphens: auto;
+  overflow-wrap: break-word;
   padding: 16px; 
   width: max-content;
-
+  word-break: break-word;
+  word-wrap: break-word;
+  
   ${(props: IStyledPopoverProps) => props.width && css`max-width: ${props.width}px`}
 `
 
