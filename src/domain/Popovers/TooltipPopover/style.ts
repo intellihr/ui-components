bb4a98dd3391751da945792db9384dc7ab60ee68
Michael Devenish <michael@michaeldevenish.com>
2018-11-02 16:15:57 +1000

    Add examples and move out to a mixin.
    [INT-3309]

--- src/domain/Popovers/TooltipPopover/style.ts
+++ src/domain/Popovers/TooltipPopover/style.ts
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components'
 import { Variables } from '../../../common'
+import { styleForLineBreakText } from '../../Typographies/services/textStyles'
 
 interface IStyledPopoverProps {
   width?: number
@@ -10,12 +11,10 @@ const StyledTooltipContent = styled.div`
   border-radius: ${Variables.Style.borderRadius}px;
   border: 1px solid ${Variables.Color.n400};
   box-shadow: 0 2px 4px rgba(38, 45, 51, 0.1);
-  overflow-wrap: break-word;
   padding: 16px; 
   width: max-content;
-  word-break: break-word;
-  word-wrap: break-word;
   
+  ${styleForLineBreakText()}
   ${(props: IStyledPopoverProps) => props.width && css`max-width: ${props.width}px`}
 `
 
