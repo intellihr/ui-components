0a53b33bceb8c581d73982e163c25112847e07e8
Michael Devenish <michael@michaeldevenish.com>
2018-11-02 14:31:08 +1000

    Remove hyphens.
    [INT-3309]

--- src/domain/Popovers/TooltipPopover/style.ts
+++ src/domain/Popovers/TooltipPopover/style.ts
@@ -10,7 +10,6 @@ const StyledTooltipContent = styled.div`
   border-radius: ${Variables.Style.borderRadius}px;
   border: 1px solid ${Variables.Color.n400};
   box-shadow: 0 2px 4px rgba(38, 45, 51, 0.1);
-  hyphens: auto;
   overflow-wrap: break-word;
   padding: 16px; 
   width: max-content;
