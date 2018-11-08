dc846d5679939b67b2000b54136e18403e45919f
Vivian Lee <vivian.lee@intellihr.com.au>
2018-11-08 13:47:50 +1000

    reverse unnessary change
    INT-3274

--- src/domain/Lists/OptionList/OptionList.tsx
+++ src/domain/Lists/OptionList/OptionList.tsx
@@ -12,7 +12,7 @@ import { Props } from '../../../common'
 
 type OptionClickCallback = (option: IOptionProps) => void
 
-type OptionValue = string | number | boolean | object
+type OptionValue = string | number | boolean
 
 interface IOptionProps {
   /** A component that is shown to the left of the text */
