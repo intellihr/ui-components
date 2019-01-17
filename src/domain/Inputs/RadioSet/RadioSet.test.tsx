161fe9fc9329899f60fda28d8eec13fe6f09212c
Andrew Hill <andrew.hill@intellihr.com.au>
2019-01-17 17:16:32 +1000

    Update RadioSet and RadioInput to work with ReduxForm
    INT-4417

--- src/domain/Inputs/RadioSet/RadioSet.test.tsx
+++ src/domain/Inputs/RadioSet/RadioSet.test.tsx
@@ -7,23 +7,19 @@ import { Props } from '../../../common'
 const dummyClick = () => console.log('hey')
 
 const exampleOptions = [{
-    name: 'option-1',
     label:'123123',
     value:'option 1'
   },
   {
-    name: 'option-2',
     label:'this is option 2 (I am disabled)',
     value:'option 2',
     isDisabled:true
   },
   {
-    name: 'option-3',
     label:'this is option 3',
     value:'option 3'
   },
   {
-    name: 'option-4',
     label:'this is option 4',
     value:'option 4'
   }]
@@ -32,7 +28,7 @@ describe('<RadioSet />', () => {
   it(`should render a radio Set`, () => {
     const wrapper = shallow(
       <RadioSet
-        id='key'
+        name='radio-input-set'
         handleChange={dummyClick}
         options={exampleOptions}
       />
@@ -45,7 +41,7 @@ describe('<RadioSet />', () => {
     const wrapper = shallow(
       <RadioSet
         useButtonStyle
-        id='key'
+        name='radio-input-set'
         handleChange={dummyClick}
         options={exampleOptions}
       />
@@ -57,7 +53,7 @@ describe('<RadioSet />', () => {
   it(`should render a radio set with horizontal buttons`, () => {
     const wrapper = shallow(
       <RadioSet
-        id='key'
+        name='radio-input-set'
         handleChange={dummyClick}
         options={exampleOptions}
         orientation={Props.Orientation.Horizontal}
