31328b58530b8c9be1c954bab94434152bc79c39
Sean Manson <sean.manson@intellihr.com.au>
2019-03-08 11:28:53 +1000

    Update to use component context
    INT-5083

--- src/domain/Spacers/HorizontalRule/HorizontalRule.tsx
+++ src/domain/Spacers/HorizontalRule/HorizontalRule.tsx
@@ -1,53 +1,24 @@
-import { isNumber } from 'lodash'
-import styled, { css, StyledFunction } from 'styled-components'
+import React from 'react'
 
-import { Variables } from '../../../common'
-import { Anchor } from '../../Internals/Anchor'
-import { IActionLinkProps } from '../../Links/ActionLink'
-
-type SpaceVariable = Variables.Layout | Variables.Spacing | 'none'
+import { Props, Variables } from '../../../common'
+import { HRSpacing, StyledHR } from './style'
 
 interface IHorizontalRuleProps {
-  topBottomSpacing?: SpaceVariable | { top: SpaceVariable, bottom: SpaceVariable }
-  leftRightSpacing?: SpaceVariable | { left: SpaceVariable, right: SpaceVariable }
+  topBottomSpacing?: HRSpacing | { top: HRSpacing, bottom: HRSpacing }
+  leftRightSpacing?: HRSpacing | { left: HRSpacing, right: HRSpacing }
+  componentContext?: string
 }
 
-function getIntendedSpacing (propSpacing: any, subSpacing: string, defaultSpacing: number): number {
-  if (isNumber(propSpacing)) {
-    return propSpacing
-  }
-
-  if (propSpacing === 'none') {
-    return 0
-  }
-
-  if (propSpacing) {
-    if (isNumber(propSpacing)) {
-      return propSpacing
-    }
-
-    if (propSpacing[subSpacing]) {
-      return (propSpacing[subSpacing] === 'none') ? 0 : propSpacing[subSpacing]
-    }
-  }
-
-  return defaultSpacing
+const HorizontalRule: React.SFC<IHorizontalRuleProps> = (props) => {
+  return (
+    <StyledHR
+      topBottomSpacing={props.topBottomSpacing || Variables.Layout.lSmall}
+      leftRightSpacing={props.leftRightSpacing || 'none'}
+      data-component-type={Props.ComponentType.HorizontalRule}
+      data-component-context={props.componentContext}
+    />
+  )
 }
-
-const HorizontalRule = styled.hr`
-  border-bottom: 1px solid ${Variables.Color.n400};
-
-  ${(props: IHorizontalRuleProps) => {
-    return css`
-      margin-top: ${getIntendedSpacing(props.topBottomSpacing, 'top', Variables.Layout.lSmall)}px;
-      margin-bottom: ${getIntendedSpacing(props.topBottomSpacing, 'bottom', Variables.Layout.lSmall)}px;
-      margin-left: ${getIntendedSpacing(props.leftRightSpacing, 'left', 0)}px;
-      margin-right: ${getIntendedSpacing(props.leftRightSpacing, 'right', 0)}px;
-    `
-  }}
-`
-
-HorizontalRule.displayName = 'HorizontalRule'
 
 export {
   IHorizontalRuleProps,
