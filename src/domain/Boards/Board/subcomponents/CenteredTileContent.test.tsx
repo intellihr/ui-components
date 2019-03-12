64981226a8b49df6c6eedb4a4d3733bd184b3fd7
Constantin Oleinic <olcod.dev@gmail.com>
2019-03-12 12:15:20 +1000

    Update test and test snapshots

--- src/domain/Boards/Board/subcomponents/CenteredTileContent.test.tsx
+++ src/domain/Boards/Board/subcomponents/CenteredTileContent.test.tsx
@@ -1,6 +1,7 @@
 import { shallow } from 'enzyme'
 import React from 'react'
 
+import { IntelliIcon } from '../../../Icons'
 import { CenteredTileContent } from './CenteredTileContent'
 
 describe('<CenteredTileContent />', () => {
@@ -26,6 +27,32 @@ describe('<CenteredTileContent />', () => {
         children
       </CenteredTileContent>
     )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a centered tile with an image, heading, subheading and a text', () => {
+    const wrapper = shallow(
+      <CenteredTileContent
+        image='an.example.url/to/an/image'
+        heading='An example heading'
+        subheading='Oh look there is a subheading'
+        text='When a merge isn’t resolved automatically, git leaves the index and the working tree in a special state that gives you all the information you need to help resolve the merge.'
+      />
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a centerd tile with an icon instead of an image', () => {
+    const wrapper = shallow(
+      <CenteredTileContent
+        iconType='intelli-icon-smile'
+        heading='Jeffrey'
+        subheading='Harmless guy'
+        text='Oh, it is a bit of this, a bit of that. It is called a Jeffrey.'
+      />
+    )
+
     expect(wrapper).toMatchSnapshot()
   })
 })
