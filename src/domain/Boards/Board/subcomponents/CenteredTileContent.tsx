ba30b1943049d919bb21be253b362a4913dfb04a
Constantin Oleinic <olcod.dev@gmail.com>
2019-03-12 11:41:57 +1000

    Update the CenteredTileContent component

--- src/domain/Boards/Board/subcomponents/CenteredTileContent.tsx
+++ src/domain/Boards/Board/subcomponents/CenteredTileContent.tsx
@@ -1,18 +1,38 @@
 import React from 'react'
 
+import { Props, Variables } from '../../../../common'
 import {
-  CenteredContentWrapper,
+  FontAwesomeIcon,
+  Icon,
+  IntelliIcon
+} from '../../../Icons'
+import {
+  Text
+ } from '../../../Typographies'
+import {
   StyledTileLabel,
   TileContentWrapper
 } from './style'
+import {
+  CenteredContentHeading,
+  CenteredContentImage,
+  CenteredContentImageWrapper,
+  CenteredContentSubheading,
+  CenteredContentText,
+  CenteredContentWrapper
+} from './styles/centeredTileContent'
 
 interface IBoardCenteredTileContentProps {
   /** Text displayed above the content of tile */
-  label?: string
+  label?: string,
+  icon?: FontAwesomeIcon | Icon | IntelliIcon,
+  image?: string,
+  heading?: string,
+  subheading?: string,
+  text?: string
 }
 
 class CenteredTileContent extends React.PureComponent<IBoardCenteredTileContentProps, never> {
-
   private get content (): JSX.Element | null {
     const {
       children,
@@ -23,7 +43,11 @@ class CenteredTileContent extends React.PureComponent<IBoardCenteredTileContentP
       <CenteredContentWrapper
         hasTitleLabel={!!label}
       >
-          {children}
+        {children}
+        {this.icon}
+        {this.heading}
+        {this.subheading}
+        {this.text}
       </CenteredContentWrapper>
     )
   }
@@ -42,6 +66,102 @@ class CenteredTileContent extends React.PureComponent<IBoardCenteredTileContentP
     }
 
     return null
+  }
+
+  private get icon (): JSX.Element | null {
+    const {
+      icon,
+      image,
+      heading
+    } = this.props
+
+    console.log(icon, image)
+    console.log(icon && image)
+    console.log(!icon && !image)
+
+    if (!icon && !image) {
+      return null
+    }
+
+    if (image) {
+      return (
+        <CenteredContentImageWrapper>
+          <CenteredContentImage
+            src={image}
+            alt={heading}
+          />
+        </CenteredContentImageWrapper>
+      )
+    }
+
+    return (
+      <CenteredContentImageWrapper>
+        {icon}
+      </CenteredContentImageWrapper>
+    )
+  }
+
+  private get heading (): JSX.Element | null {
+    const {
+      heading
+    } = this.props
+
+    if (heading === undefined) {
+      return null
+    }
+
+    return (
+      <CenteredContentHeading>
+        <Text
+          isInline={false}
+          type={Props.TypographyType.DisplayLarge}
+          color={Variables.Color.inherit}
+        >
+          {heading}
+        </Text>
+      </CenteredContentHeading>
+    )
+  }
+
+  private get subheading (): JSX.Element | null {
+    const {
+      subheading
+    } = this.props
+
+    if (subheading === undefined) {
+      return null
+    }
+
+    return (
+      <CenteredContentSubheading>
+        <Text
+          isInline={false}
+          type={Props.TypographyType.Body}
+          color={Variables.Color.inherit}
+          isUpper
+        >
+          {subheading}
+        </Text>
+      </CenteredContentSubheading>
+    )
+  }
+
+  private get text (): JSX.Element | null {
+    const {
+      text
+    } = this.props
+
+    if (text === undefined || text === '') {
+      return null
+    }
+
+    return (
+      <CenteredContentText>
+        <Text isInline={false} type={Props.TypographyType.Small}>
+          {text}
+        </Text>
+      </CenteredContentText>
+    )
   }
 
   public render (): JSX.Element {
