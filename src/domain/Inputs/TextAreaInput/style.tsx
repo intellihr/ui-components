76137c4fcc24e271d1ca400a08e725210aab18ea
Andrew Tang <andrew.tang@intellihr.co>
2019-06-03 15:55:25 +1000

    Define all of the props for IStyledAutosizeTextAreaProps
    [INT-6368]

--- src/domain/Inputs/TextAreaInput/style.tsx
+++ src/domain/Inputs/TextAreaInput/style.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import AutosizeTextarea from 'react-autosize-textarea'
 import styled, { StyledFunction } from 'styled-components'
-
 import { Variables } from '../../../common'
+
+interface IStyledAutosizeTextAreaProps {
+  /** Custom classname to use */
+  className?: string
+  /** ID of the input */
+  id?: string
+  /** Name of the input */
+  name: string
+  /** Value change handler */
+  onChange?: React.ChangeEventHandler<HTMLTextAreaElement>
+  /** Value of the input */
+  value?: string
+  /** If true, sets input to disabled state */
+  disabled?: boolean
+  /** Number of rows to initially display */
+  rows?: number
+  /** Placeholder text to display when input is empty */
+  placeholder?: string
+  /** If true, use HTML5 required attribute */
+  required?: boolean
+  /** If the necessary props are passed in to enable gifs */
+  gifsEnabled?: boolean
+  /** If a gif has been selected */
+  hasGif?: boolean
+}
 
 interface IStyledGifButtonProps {
   /** If the popover gif list is opened */
   opened: boolean
-}
-
-interface IStyledAutosizeTextAreaProps {
-  /** If the necessary props are passed in to enable gifs */
-  gifsEnabled: boolean
-  /** If a gif has been selected */
-  hasGif: boolean
 }
 
 const StyledMainGifContainer = styled.div`
