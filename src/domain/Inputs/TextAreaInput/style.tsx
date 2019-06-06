62a3da80fd179b2ed46e61226a78618c38097f73
Andrew Tang <andrew.tang@intellihr.co>
2019-06-06 13:37:24 +1000

    Remove accidental inclusion
    [INT-6088]

--- src/domain/Inputs/TextAreaInput/style.tsx
+++ src/domain/Inputs/TextAreaInput/style.tsx
@@ -74,10 +74,6 @@ const StyledAutosizeTextarea = textArea`
     border-color: ${Variables.Color.i400};
   }
 
-  &.is-invalid-input:not(:focus) + ${StyledMainGifContainer} {
-    border-color: ${Variables.Color.r400};
-  }
-
   &:disabled,
   &[disabled] {
     color: ${Variables.Color.n600}
