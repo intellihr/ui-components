45619b4daf231b8c6b91f4c461c0214b34137f39
Vivian Lee <vivian.lee@intellihr.com.au>
2019-11-18 11:57:18 +1000

    Fix modal with long heading
    INT-8708

--- src/domain/Modals/Modal/subcomponents/style.ts
+++ src/domain/Modals/Modal/subcomponents/style.ts
@@ -1,10 +1,9 @@
-import React from 'react'
 import styled from 'styled-components'
 
 import { Variables } from '../../../../common'
 
 const StyledModalHeader = styled.div`
-  height: 80px;
+  min-height: 80px;
   background-color: ${Variables.Color.n200};
   padding: 24px;
 `
