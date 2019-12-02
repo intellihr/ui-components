9c99632d784ebdeb6ae0f83a0f8761cd87e96aa1
Michael Devenish <michael@michaeldevenish.com>
2019-12-02 11:09:42 +1000

    Create Margin ui component.
    [INT-8886]

---
+++ src/domain/Spacers/Margin/Margin.tsx
@@ -0,0 +1,23 @@
+import { isEqual } from 'lodash'
+import React from 'react'
+
+import { Props } from '../../../common'
+import { StyledMargin } from './style'
+
+const Margin: React.FC<{
+  margins: Props.IMargins
+  children?: React.ReactNode
+}> = ({margins, children}) =>  (
+  <StyledMargin
+    margins={margins}
+  >
+    {children}
+  </StyledMargin>
+)
+
+const MemoMargin = React.memo(Margin, (prevProps, nextProps) => isEqual(prevProps.margins, nextProps.margins))
+MemoMargin.displayName = 'Margin'
+
+export {
+  MemoMargin as Margin
+}
--- src/domain/Spacers/index.ts
+++ src/domain/Spacers/index.ts
@@ -1,1 +1,2 @@
 export * from './HorizontalRule'
+export * from './Margin'
