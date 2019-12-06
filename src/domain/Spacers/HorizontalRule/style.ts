267a41727c23b3ed01da18be52374a9710d8af68
Vivian Lee <vivian.lee@intellihr.com.au>
2019-12-06 15:48:11 +1000

    Update horizontal rule component to use margins
    INT-8809

--- src/domain/Spacers/HorizontalRule/style.ts
+++ src/domain/Spacers/HorizontalRule/style.ts
@@ -1,49 +1,18 @@
-import { isNumber } from 'lodash'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
-import { Variables } from '../../../common'
-
-type HRSpacing = Variables.Layout | Variables.Spacing | 'none'
+import { Props, Variables } from '../../../common'
+import { styleForMargins } from '../services/margins'
 
 interface IStyledHRProps {
-  topBottomSpacing: HRSpacing | { top: HRSpacing, bottom: HRSpacing }
-  leftRightSpacing: HRSpacing | { left: HRSpacing, right: HRSpacing }
-}
-
-function getIntendedSpacing (propSpacing: any, subSpacing: string): number {
-  if (isNumber(propSpacing)) {
-    return propSpacing
-  }
-
-  if (propSpacing === 'none') {
-    return 0
-  }
-
-  if (isNumber(propSpacing)) {
-    return propSpacing
-  }
-
-  if (propSpacing[subSpacing]) {
-    return (propSpacing[subSpacing] === 'none') ? 0 : propSpacing[subSpacing]
-  }
-
-  return 0
+  margins?: Props.IMargins
 }
 
 const StyledHR = styled.hr<IStyledHRProps>`
   border-bottom: 1px solid ${Variables.Color.n400};
 
-  ${(props: IStyledHRProps) => {
-  return css`
-      margin-top: ${getIntendedSpacing(props.topBottomSpacing, 'top')}px;
-      margin-bottom: ${getIntendedSpacing(props.topBottomSpacing, 'bottom')}px;
-      margin-left: ${getIntendedSpacing(props.leftRightSpacing, 'left')}px;
-      margin-right: ${getIntendedSpacing(props.leftRightSpacing, 'right')}px;
-    `
-}}
+  ${(props: IStyledHRProps) => styleForMargins(props.margins)}
 `
 
 export {
-  StyledHR,
-  HRSpacing
+  StyledHR
 }
