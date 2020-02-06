85832d006b29318ee3ed2328644a9c9635c3f019
Michael Devenish <michael@michaeldevenish.com>
2020-02-06 11:06:01 +1000

    Port old menu item to legacy menu item.
    [INT-9837]

---
+++ src/domain/Menus/LegacyMenu/MenuItem/MenuItem.test.tsx
@@ -0,0 +1,101 @@
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import { IntelliIcon } from '../../../Icons'
+import { MenuItem } from './MenuItem'
+
+describe('<MenuItem />', () => {
+  it('should render a simple menu item', () => {
+    const wrapper = shallow(
+      <MenuItem
+        label='Test menu item'
+      />
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a a menu item with a link', () => {
+    const wrapper = shallow(
+      <MenuItem
+        href='www.google.com'
+        label='Test menu item'
+      />
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a menu item with active state', () => {
+    const wrapper = shallow(
+      <MenuItem
+        href='www.google.com'
+        label='Test menu item'
+        className='active'
+      />
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a menu item with Icon', () => {
+    const wrapper = shallow(
+      <MenuItem
+        href='www.google.com'
+        label='Test menu item'
+        icon={<IntelliIcon icon='clock' />}
+      />
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a menu item with loading state', () => {
+    const wrapper = shallow(
+      <MenuItem
+        href='www.google.com'
+        label='Test menu item'
+        icon={<IntelliIcon icon='clock' />}
+        isLoading
+      />
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a menu item as a submenu if it has children', () => {
+    const wrapper = shallow(
+      <MenuItem
+        href='www.google.com'
+        label='Father'
+        icon={<IntelliIcon icon='clock' />}
+      >
+        <MenuItem
+          href='www.google.com'
+          label='Son'
+          icon={<IntelliIcon icon='clock' />}
+        />
+        <MenuItem
+          href='www.google.com'
+          label='Devil'
+          icon={<IntelliIcon icon='clock' />}
+        />
+      </MenuItem>
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a menu item which overflowWhenOpen prop is passed to the Collapsible component', () => {
+    const wrapper = shallow(
+      <MenuItem
+        label='Test menu item'
+        overflowWhenOpen='visible'
+      >
+        Something
+      </MenuItem>
+    )
+
+    expect(wrapper).toMatchSnapshot()
+  })
+})
