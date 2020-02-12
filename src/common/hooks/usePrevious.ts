26039b370972d672492f4db1a60c99bace42e9ac
aytee17 <andrew.tang@outlook.com>
2020-02-12 18:00:46 +1000

    Update BlockTabGroup to accept icons in tabs and add option for tab width to match the widest tab
    [INT-9802]

---
+++ src/common/hooks/usePrevious.ts
@@ -0,0 +1,15 @@
+import { useEffect, useRef } from 'react'
+
+function usePrevious<T> (value: T) {
+  const ref = useRef<T>()
+
+  useEffect(() => {
+    ref.current = value
+  })
+
+  return ref.current
+}
+
+export {
+  usePrevious
+}
