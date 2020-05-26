8d34c97bc0a68ab0c0e842f73bf916fe002b6a48
Michael Devenish <michael@michaeldevenish.com>
2020-05-26 11:54:07 +1000

    Export styleForMargins.
    [MINOR]

--- src/domain/Spacers/Margin/Margin.tsx
+++ src/domain/Spacers/Margin/Margin.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import { FlattenSimpleInterpolation } from 'styled-components'
 
 import { Props } from '../../../common'
+import { styleForMargins } from '../services/margins'
 import { StyledMargin } from './style'
 
-const Margin: React.FC<{
+type IMargin = React.FC<{
   margins?: Props.IMargins
   children?: React.ReactNode
-}> = ({margins, children}) =>  (
+}> & {
+  styleForMargins: (margins?: Props.IMargins) => FlattenSimpleInterpolation | null
+}
+
+const Margin: IMargin = ({margins, children}) =>  (
   <StyledMargin
     margins={margins}
   >
     {children}
   </StyledMargin>
 )
+
+Margin.styleForMargins = styleForMargins
 
 export {
   Margin
