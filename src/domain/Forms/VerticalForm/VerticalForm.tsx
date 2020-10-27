27bac12fecb18b62e7c33e70ea26d95cfee53c24
Andrew Hill <andrew.hill@intellihr.com.au>
2020-10-27 11:19:02 +1000

    Clean up form section margins
    intellihr/engineering/team-kiki/team-kiki#14457

--- src/domain/Forms/VerticalForm/VerticalForm.tsx
+++ src/domain/Forms/VerticalForm/VerticalForm.tsx
@@ -1,9 +1,11 @@
+import { findLastIndex } from 'lodash'
 import React from 'react'
 
 import { Field } from './subcomponents/Field'
 import { LeftAlignControls } from './subcomponents/LeftAlignControls'
 import { RightAlignControls } from './subcomponents/RightAlignControls'
 import { Section } from './subcomponents/Section'
+import { StyledSectionWrapper } from './subcomponents/Section/style'
 
 class VerticalForm extends React.PureComponent<React.FormHTMLAttributes<HTMLFormElement>> {
   public static Field = Field
@@ -21,6 +23,27 @@ class VerticalForm extends React.PureComponent<React.FormHTMLAttributes<HTMLForm
       autoComplete,
       children
     } = this.props
+    let wrappedChildren = children
+
+    const childrenArray = React.Children.toArray(children)
+    const lastSectionIndex = findLastIndex(childrenArray, (child: any) => {
+      return child && child?.type && child?.type?.name === Section.name
+    })
+
+    if (lastSectionIndex >= 0) {
+      wrappedChildren = React.Children.map(
+        children,
+        (child: any, index) => {
+          if ((lastSectionIndex !== index) && child && child?.type && child?.type?.name === Section.name) {
+            return (
+              <StyledSectionWrapper>{child}</StyledSectionWrapper>
+            )
+          }
+
+          return child
+        }
+      )
+    }
 
     return (
       <form
@@ -28,7 +51,7 @@ class VerticalForm extends React.PureComponent<React.FormHTMLAttributes<HTMLForm
         onChange={onChange}
         autoComplete={autoComplete}
       >
-        {children}
+        {wrappedChildren}
       </form>
     )
   }
--- src/domain/Forms/VerticalForm/subcomponents/Section/style.ts
+++ src/domain/Forms/VerticalForm/subcomponents/Section/style.ts
@@ -2,9 +2,9 @@ import styled, { css } from 'styled-components'
 
 import { Variables } from '../../../../../common'
 
-const StyledSection = styled.div`
-  margin-bottom: ${Variables.Spacing.s3XLarge}px;
+const StyledSectionWrapper = styled.div`
+  margin-bottom: ${Variables.Spacing.sXLarge}px;
 `
 export {
-  StyledSection
+  StyledSectionWrapper
 }
